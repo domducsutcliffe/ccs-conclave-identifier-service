19bfdcd6c126ad9db3d94b2c782038b118e455a8
domducsutcliffe <dom.sutcliffe@crowncommercial.gov.uk>
2023-03-01 10:43:15 +0000

    Added routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get "/identifiers/id/:id_type", to: "ppon_identifier#index"
-
-  namespace :testing do
-    get "/ppon_get_first", to: "testing#return_first_ppon_id"
-    get "/ppon_get/:index", to: "testing#return_specific_ppon_id"
+  namespace 'api' do
+    namespace 'v1' do
+      resources :organisations
+    end
   end
 end
