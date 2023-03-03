b7453332569cb3f2aa574e00c32cb8b59cbc6672
domducsutcliffe <dom.sutcliffe@crowncommercial.gov.uk>
2023-03-03 16:21:02 +0000

    Removing Org ID from response gen

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 Organisation.destroy_all
 Organisation.reset_pk_sequence
 Organisation.create!([{
-    PPON_ID: "AA1111AA",
+    PPON_ID: "AA1111AA1",
 }])
 
 p "Created #{Organisation.count} Organisations"
