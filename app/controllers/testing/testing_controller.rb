5f80c5b4949509cc13684d36948c6af0f0c62597
@tberey <tomberey1@gmail.com>
2023-03-13 14:57:24 +0000

    still-fixing-deploy-pipeline

--- app/controllers/testing/testing_controller.rb
+++ app/controllers/testing/testing_controller.rb
@@ -4,20 +4,20 @@ module Testing
   class TestingController < ActionController::API
 
     def return_first_ppon_id # AA1111AA1
-      #render json: { "PPON_ID": Helpers::ApplicationHelper.generate_specific_ppon_id(0) }, status: :ok
+      render json: { "PPON_ID": Helpers::ApplicationHelper.generate_specific_ppon_id(0) }, status: :ok
     end
 
     def return_second_ppon_id # AA1111AA2
-      #render json: { "PPON_ID": Helpers::ApplicationHelper.generate_specific_ppon_id(1) }, status: :ok
+      render json: { "PPON_ID": Helpers::ApplicationHelper.generate_specific_ppon_id(1) }, status: :ok
     end
 
     def return_third_ppon_id # AA1111AA3
-      #render json: { "PPON_ID": Helpers::ApplicationHelper.generate_specific_ppon_id(2) }, status: :ok
+      render json: { "PPON_ID": Helpers::ApplicationHelper.generate_specific_ppon_id(2) }, status: :ok
     end
     
     def return_specific_ppon_id
-      #index = params[:index]
-      #render json: { "PPON_ID": Helpers::ApplicationHelper.generate_specific_ppon_id(index) }, status: :ok
+      index = params[:index]
+      render json: { "PPON_ID": Helpers::ApplicationHelper.generate_specific_ppon_id(index) }, status: :ok
     end
   end
 end
