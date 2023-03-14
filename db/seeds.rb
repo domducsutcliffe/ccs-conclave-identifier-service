672d651481ee9e251e6c02addfb6dd892f6a5ff2
domducsutcliffe <dom.sutcliffe@crowncommercial.gov.uk>
2023-03-14 11:27:28 +0000

    Clean version of app

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-Organisation.destroy_all
-Organisation.reset_pk_sequence
-Organisation.create!([{
-    PPON_ID: "AA1111AA1",
-}])
-
-p "Created #{Organisation.count} Organisations"
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
