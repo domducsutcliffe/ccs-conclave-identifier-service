4dde0e2310f6da5e119eabb9b4f225705f9b1495
domducsutcliffe <dom.sutcliffe@crowncommercial.gov.uk>
2023-03-20 17:45:13 +0000

    Making route include PPON URL

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
-      resources :organisations, only: [:index, :show, :create]
+      match '/ppon', to: 'organisations#create', via: [:get]
     end
   end
 end
