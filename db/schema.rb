0ac1905ad14096f0fa92881e87be7642aade69b7
Sam Boothroyd <sam.boothroyd@co-cc02771.ad.cabinetoffice.gov.uk>
2023-03-20 16:15:37 +0000

    Remove updated_at column from database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_15_114134) do
+ActiveRecord::Schema.define(version: 2023_03_20_151644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 2023_03_15_114134) do
   create_table "organisations", force: :cascade do |t|
     t.string "ppon_id"
     t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
