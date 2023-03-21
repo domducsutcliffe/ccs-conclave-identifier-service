c973b8ec54f679af287b3ad20075c74a318d8236
domducsutcliffe <dom.sutcliffe@crowncommercial.gov.uk>
2023-03-21 16:36:08 +0000

    App now handles 404 errors as spec specifies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+    def not_found
+        render json:
+        [ identifiers:
+            [
+                {
+                    "id": "string",
+                    "id-type": "string",
+                    "persisted": "string"
+                }
+            ], 
+            "description": "The resource you were looking for could not be found."
+        ], status: :not_found
+    end
 end
