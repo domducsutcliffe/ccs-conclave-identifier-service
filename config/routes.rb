c62158034a96ed65a489f426c3cf245f50ffb7ea
domducsutcliffe <dom.sutcliffe@crowncommercial.gov.uk>
2023-03-22 11:47:02 +0000

    typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
-      match '/organisation', to: 'organisations#create', via: [:post]
+      match '/organisations', to: 'organisations#create', via: [:post]
     end
   end
   get '*unmatched_route', to: 'application#not_found'
