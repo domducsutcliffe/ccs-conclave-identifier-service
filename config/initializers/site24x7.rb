a206fdefc3d2c3fecb75ed5c8138cf3fa9805231
domducsutcliffe <dom.sutcliffe@crowncommercial.gov.uk>
2023-05-24 13:13:32 +0100

    New config

---
+++ config/initializers/site24x7.rb
@@ -0,0 +1,10 @@
+require 'erb'
+
+# Load the ERB template
+template = ERB.new(File.read("#{Rails.root}/app/apminsight.conf.erb"))
+
+# Render the template with the current binding
+result = template.result(binding)
+
+# Write the result to the apminsight.conf file
+File.write("#{Rails.root}/app.apminsight.conf", result)
